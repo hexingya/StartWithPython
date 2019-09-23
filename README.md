2c8d5890af9d9a09f94badc0e6f93fe5085f062e
Avi <avi@localhost.localdomain>
2019-09-23 13:12:11 +0530

    Python Introduction

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # The Way Of Programing - Python
+
+Github repo: `introduction`
+
+Youtube Session: [https://youtu.be/bysjZ_p4QHM]
+
+- Python Introduction
+- Number Literal
+- Casting
+- String Literal
+- String Concatenation
+- String Slicing
+- Boolean Literals
+- A glimps of operators
---
+++ introduction/demo.py
@@ -0,0 +1,109 @@
+# print statement
+# another comment
+
+# number = 45
+
+# print('Value: ' + str(number))
+
+# print('Mohan' + ' ' + 'Lal')
+
+# to convert a number into a string
+# number = str(number)
+
+# print(type(number))
+
+# string_value = int("3545")
+
+# print(type(string_value))
+
+# string_value = float("3545")
+
+# print(string_value)
+
+# int(), float(), str()
+
+# num = 4554
+# print(type(num))
+
+# num = str(num)
+# print(type(num))
+
+
+# numbers
+# int
+# float
+# complex
+
+# x = 2+3j
+
+# print(type(x))
+# print(x)
+
+# we cannot convert complex into int or float
+# x = float(x)
+# print(type(x))
+# value = float("56")
+# number1 = int(value)
+
+# number1 = complex(number1)
+# print(type(number1))
+# print(number1)
+
+# string...
+# concatination (+) & repetition (*)
+
+# your_name = 'Rama'
+
+# print("Your name is " + your_name + '.')
+
+# print('app' + your_name)
+
+# print("Ramu " * 10)
+
+# print('* '*100)
+# print((your_name + " ") * 15)
+
+# print("Planet" * int("7"))
+# print("Planet" * 7)
+
+# string can be indexed
+
+# message = "I have a message"
+
+# print(message[-32])
+
+# string immutable (cannot be changed)
+
+# new_string = "Something"
+
+# error
+# new_string[5] = 'Y'
+
+# string slicing
+message = "We are going to slice this string."
+
+# print(message[0:8])
+# print(message[:10])
+# print(message[10:])
+
+# string length
+
+# print(len(message))
+# print(len('app'))
+
+# operators
+
+# arithmetic
+#  + - * / % // **
+
+print(5 // 2)
+print(9 // 3)
+print(9 / 4)
+print(8 ** 4)
+
+# logical op --  and or not
+# == != < > <= >=
+
+# ......
+
+#  to be continued...
